a448528f5a3465274ca0de4d778f7f79c57cb6f7
juandebravo <juandebravo@gmail.com>
2013-06-25 01:04:21 +0200

    minor style changes

--- js/template-ui.js
+++ js/template-ui.js
@@ -1,75 +1,70 @@
-'use strict';
 
-const UI = (function() {
+(function() {
+  'use strict';
   navigator.mozL10n.ready(function localize() {
     // Do nothing rigth now
   });
 
-  var settingsView = document.querySelector('#settings-view');
+  var settingsView = document.querySelector('#settings-view'),
+      settingsOpen = document.querySelector('#settings-open'),
+      settingsDone = document.querySelector('#settings-done'),
+      startButton = document.querySelector('#start'),
+      alarmSound = new Howl({urls: ['res/alarm.wav']}),
+      coinSound = new Howl({urls: ['res/coindrop.wav']}),
+      current_cost = 0,
+      running = false,
+      minutes = 0,
+      seconds = 0,
+      task,
+      lastCoinSound = 0;
 
-  var settingsOpen = document.querySelector('#settings-open');
   settingsOpen.addEventListener('click', function(e) {
     settingsView.dataset.pagePosition = 'viewport';
   });
 
-  var settingsDone = document.querySelector('#settings-done');
+
   settingsDone.addEventListener('click', function(e) {
     settingsView.dataset.pagePosition = 'bottom';
   });
-
-  var alarmSound = new Howl({
-    urls: ['res/alarm.wav']
-  });
-  
-  var coinSound = new Howl({
-    urls: ['res/coindrop.wav']
-  });
-
-  var task;
-  var current_cost = 0;
-  var running = false;
-  var minutes = 0;
-  var seconds = 0;
 
   var resetCounters = function() {
     current_cost = 0;
     minutes = 0;
     seconds = 0;
-  }
+  };
 
   var incrementTotal = function(count) {
     asyncStorage.getItem('historic', function(value) {
       value = value || 0;
       asyncStorage.setItem('historic', value + count, function() {
-        renderTotal();  
+        renderTotal();
       });
     });
-  }
+  };
 
   var renderTotal = function() {
     asyncStorage.getItem('historic', function(value) {
       value = value || 0;
       document.querySelector('#total').innerHTML = value + ' minutes';
     });
-  }
+  };
 
   var pad = function(num) {
     return (num < 10) ? '0' + num.toString() : num.toString();
-  }
+  };
 
-  var lastCoinSound = 0;
   var incrementCost = function(sec) {
     current_cost += sec * 60 / 3600; // 1 min = 1 eur (60 eur/h avg)
 
     var roundedCost = Math.floor(current_cost);
 
-    if (lastCoinSound != roundedCost && roundedCost % 5 === 0) {
+    if (lastCoinSound !== roundedCost && roundedCost % 5 === 0) {
       lastCoinSound = roundedCost;
       coinSound.play();
     }
 
     document.querySelector('.current_cost').innerHTML = current_cost.toFixed(2) + ' EUR / head';
-  }
+  };
 
   var incrementCurrentTime = function(sec) {
     seconds += sec;
@@ -82,7 +77,7 @@ const UI = (function() {
     }
 
     document.querySelector('.current_minutes').innerHTML = pad(minutes) + ':' + pad(seconds);
-  }
+  };
 
   // TODO keep in foreground?
 
@@ -91,33 +86,32 @@ const UI = (function() {
     if (running) {
       if (document.mozHidden) {
         backgroundDate = new Date();
-
-        stopTrackingTask();
+        stopTrackingTask(task);
       } else {
         var currentDate = new Date();
         var secondsInBackground = (currentDate.getTime() - backgroundDate.getTime()) / 1000;
         secondsInBackground = Math.round(secondsInBackground);
-
-        incrementCurrentTime(secondsInBackground);
-        incrementCost(secondsInBackground);
-
-        startTrackingTask();
+        incrementFacade(secondsInBackground);
+        task = startTrackingTask();
       }
     }
   });
 
+  var incrementFacade = function(time) {
+    incrementCurrentTime(time);
+    incrementCost(time);
+  };
+
   var startTrackingTask = function() {
-    task = setInterval(function() {
-        incrementCurrentTime(1);
-        incrementCost(1);
+    return setInterval(function() {
+        incrementFacade(1);
       }, 1000);
-  }
+  };
 
-  var stopTrackingTask = function() {
+  var stopTrackingTask = function(task) {
     clearInterval(task);
-  }
+  };
 
-  var startButton = document.querySelector('#start');
   startButton.addEventListener('click', function(e) {
     running = !running;
 
@@ -127,14 +121,12 @@ const UI = (function() {
       resetCounters();
 
       startButton.className = 'danger';
-      startButton.innerHTML = 'Stop Meeting'
-
-      startTrackingTask();
+      startButton.innerHTML = 'Stop Meeting';
+      task = startTrackingTask();
     } else {
       startButton.className = 'recommend';
-      startButton.innerHTML = 'Start Meeting';      
-
-      stopTrackingTask();
+      startButton.innerHTML = 'Start Meeting';
+      stopTrackingTask(task);
     }
   });
 
